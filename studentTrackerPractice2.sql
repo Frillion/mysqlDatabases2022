e8c34f3d15655a53da2ae5923b849917233575e5
Frillion <fridirksoebech@gmail.com>
2022-03-18 09:57:48 +0000

    worked on the first problem a bit+

--- studentTrackerPractice2.sql
+++ studentTrackerPractice2.sql
@@ -1,2 +1,14 @@
 use studytracker
 delimiter $$
+drop trigger if exists check_student_status$$
+create trigger check_student_status 
+before insert on registration
+for each row
+begin
+	declare msg varchar(128);
+	if (select StudentStatus(new.studentID))
+		then set msg = 'Student is not registered as an attendee in the studentstatus table';
+		signal sqlstate '45000' set message_text = msg;
+	end if;
+end
+
