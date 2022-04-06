3ca059e9e2957027838558cdab9dc94ed372129a
Frillion <fridriksoebech@gmail.com>
2022-04-06 01:06:59 +0000

    Finished all CRUD storerd procedures that are needed on the database and all CRUD operations for DBmanager, also added a psudoCode text file which houses the psudo code used to construct the mysql script

--- APIS/db-manager-test.py
+++ APIS/db-manager-test.py
@@ -3,4 +3,3 @@ import db_manager
 manager = db_manager.DbManager()
 
 print(manager.status)
-course = manager.single_course()
--- APIS/db_manager.py
+++ APIS/db_manager.py
@@ -69,44 +69,105 @@ class DbManager:
 
 
     # ---------------------------------------------------------------------------------------
-    def add_track_course(self, course_number, semester_id, is_mandatory):
-        course = {
+    def add_track_course(self, track_id, course_number, semester_id, is_mandatory):
+        track_course = {
+            "track_id":track_id,
             "course_number":course_number,
             "semester_id":semester_id,
             "is_mandatory":is_mandatory
         }
-        param = [json.dumps(course, ensure_ascii=False)]
+        param = [json.dumps(track_course, ensure_ascii=False)]
         response = self.execute_sql_procedure('add_track_course_json', param)
         return json.loads(response[0][0])
         pass
 
 
     def single_track_course(self, track_id, course_number):
+        track_course = {
+            "track_id":track_id,
+            "course_number":course_number
+        }
+        param = [json.dumps(track_course, ensure_ascii=False)]
+        response = self.execute_sql_procedure('get_track_course_json', param)
+        try:
+            return json.loads(response[0][0])
+        except IndexError:
+            return response
         pass
 
 
-    def update_track_course(self, track_id, course_number, semester_id, is_mandatory):
+    def update_track_course(self, track_id, course_number, semester_id=None, is_mandatory=None):
+        track_course = {
+            "track_id":track_id,
+            "course_number":course_number,
+            "semester_id":semester_id,
+            "is_mandatory":is_mandatory
+        }
+        param = [json.dumps(track_course, ensure_ascii=False)]
+        response = self.execute_sql_procedure('update_track_course_json', param)
+        return json.loads(response[0][0])
         pass
 
 
     def delete_track_course(self, track_id, course_number):
+        track_course = {
+            "track_id":track_id,
+            "course_number":course_number
+        }
+        param = [json.dumps(track_course, ensure_ascii=False)]
+        response = self.execute_sql_procedure('delete_track_course_json',param)
+        return json.loads(response[0][0])
         pass
 
 
     # ---------------------------------------------------------------------------------------
     def add_semester(self, semester_name, semester_starts, semester_ends, academic_year):
+        semester={
+            "semester_name":semester_name,
+            "semester_start":semester_starts,
+            "semester_end":semester_ends,
+            "academic_year":academic_year
+        }
+        param = [json.dumps(semester, ensure_ascii=False)]
+        response = self.execute_sql_procedure('add_semester_json',param)
+        return json.loads(response[0][0])
         pass
 
 
     def single_semester(self, semester_id):
+        semester={
+            "semester_id":semester_id
+        }
+        param = [json.dumps(semester, ensure_ascii=False)]
+        response = self.execute_sql_procedure('get_semester_json',param)
+        try:
+            return json.loads(response[0][0])
+        except IndexError:
+            return response
         pass
 
 
-    def update_semester(self, semester_id, semester_name, semester_starts, semester_ends, academic_year):
+    def update_semester(self, semester_id, semester_name=None, semester_starts=None, semester_ends=None, academic_year=None):
+        semester = {
+            "semester_id":semester_id,
+            "semester_name":semester_name,
+            "semester_start":semester_starts,
+            "semester_end":semester_ends,
+            "academic_year":academic_year
+        }
+        param = [json.dumps(semester, ensure_ascii=False)]
+        response = self.execute_sql_procedure('update_semester_json', param)
+        return json.loads(response[0][0])
         pass
 
 
     def delete_semester(self, semester_id):
+        semester={
+            "semester_id":semester_id
+        }
+        param = [json.dumps(semester, ensure_ascii=False)]
+        response = self.execute_sql_procedure('delete_semester_json', param)
+        return json.loads(response[0][0])
         pass
 
 
