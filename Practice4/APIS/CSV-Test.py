248d7ff0b43b4b556d0afb854f1b123e9e90fb14
Frillion <fridirksoebech@gmail.com>
2022-05-04 13:52:24 +0000

    All procedures have been finished

--- Practice4/APIS/CSV-Test.py
+++ Practice4/APIS/CSV-Test.py
@@ -1,4 +1,4 @@
 from CSV_Import import *
 
 dbmanager = DbManager()
-dbmanager.insert_entry_from_csv('D:\School2022\Databases\Practice4\\Mannfjoldi_a_islandi.csv',False)
+dbmanager.insert_entry_from_csv('D:\Database2022\Practice4\Mannfjoldi_a_islandi.csv',True)
--- Practice4/population_tracker_operations.sql
+++ Practice4/population_tracker_operations.sql
@@ -25,13 +25,48 @@ begin
     where recordDate = record_date
 	order by cityid;
 end$$
+/*	This Procedure Is Both Number 4 and 5 In The Project.
+	It returns either an overview of growth of all cities or just one
+    depending on wether the city_id field is null or not
+*/
 drop procedure if exists percentage_growth$$
-create procedure percentage_growth(date_from date,date_to date)
+create procedure percentage_growth(date_from date,date_to date,c_id char(4),r_name varchar(75))
 get_percentage:begin
+    declare sum_pop_second int;
 	if datediff(date_to,date_from) < 0
     then leave get_percentage;
     end if;
-    select c.cityid,cityname,(select population from population where cityid = c.cityid and recorddate = date_to)-population as NumberDiff,1 - (population/(select population from population where cityid = c.cityid and recorddate = date_to)) as PercentageGrowth from cities c
-    join population p on p.cityid = c.cityid
-    where recorddate = date_from;
+    if c_id = '' and r_name = ''
+		then select c.CityID,CityName,RecordDate,Population,(select RecordDate from population where recorddate = date_to and cityID = c.cityID)as RecordDate,(select population from population where recorddate = date_to and cityID = c.cityID) as Population,(select population from population where cityid = c.cityid and recorddate = date_to)-population as NumberDiff,1 - (population/(select population from population where cityid = c.cityid and recorddate = date_to)) as PercentageGrowth from cities c
+		join population p on p.cityid = c.cityid
+		where recorddate = date_from;
+    else if r_name = ''
+		then select c.CityID,CityName,RecordDate,Population,(select RecordDate from population where recorddate = date_to and cityID = c.cityID)as RecordDate,(select population from population where recorddate = date_to and cityID = c.cityID) as Population,(select population from population where cityid = c.cityid and recorddate = date_to)-population as NumberDiff,1 - (population/(select population from population where cityid = c.cityid and recorddate = date_to)) as PercentageGrowth from cities c
+		join population p on p.cityid = c.cityid
+		where recorddate = date_from and c.CityID = c_id;
+	else
+		Set sum_pop_second = (select sum(population) from regions r
+			join cities c on r.RegionID = c.RegionID
+			join population p on p.CityID = c.CityID
+			where RegionName = r_name and recorddate = date_to);
+            
+		select RegionName,RecordDate,sum(population) as Population,
+		(select RecordDate from population where recorddate = date_to and cityID = c.cityID)as RecordDate,(select sum_pop_second) as Population,(select sum_pop_second)-sum(population) as NumberDiff,1 - (sum(population)/(select sum_pop_second)) as PercentageGrowth from regions r
+        join cities c on r.RegionID = c.RegionID
+		join population p on p.cityid = c.cityid
+		where recorddate = date_from and RegionName = r_name;
+        end if;
+    end if;
 end$$
+drop procedure if exists get_region_pop$$
+create procedure get_region_pop(region_name varchar(75))
+begin
+	select RegionName,RecordDate,sum(population) from regions r
+    join cities c on r.RegionID = c.RegionID
+    join population p on p.CityID = c.CityID
+    where RegionName = region_name
+    group by RecordDate;
+end$$
+
+call percentage_growth("2017-12-01","2018-04-15","","Höfuðborgarsvæðið");
+#call get_region_pop((select RegionName from regions where regionid = 3));
